07bb9961cf6e9981100a2232704e788d4f78d09a
nish0753 <130308428+nish0753@users.noreply.github.com>
2024-06-03 18:15:33 +0000

    datatype

--- .devcontainer/basics/conversionOperation.js
+++ .devcontainer/basics/conversionOperation.js
@@ -28,3 +28,24 @@ let stringNumber = String(someNumber)
 console.log(someNumber);
 
 console.log(typeof stringNumber)
+//************* operations ************ //
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(2 + 1 + "1");
+// Agr string phle rha to baki sbko bhi string ki trh hi treat kiya jayega 
+//or agr string bad me hua to phle jo operation hain wo honge fir string as it is aayega 
+
+
+let x = 3;
+const y = x++;//postfix increment(bad me icrease krta hai)
+console.log(x);
+
+//output :- x= 4; and y =3;
+
+let z = 4;
+const a = ++z;//prefix increment (phle increase krta hai )
+console.log(z);
+
+//output :- z= 5 and a = 5;
+//
