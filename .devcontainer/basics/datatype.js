f18f596065f5dfec304599181601e0ede57288cf
nish0753 <130308428+nish0753@users.noreply.github.com>
2024-06-03 04:36:40 +0000

    datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,27 @@
+"use strict" //Treat all js code as newer verison 
+
+//alert(3+3) we are using node js not browser;
+
+console.log(3+3);
+console.log("Nishant");
+
+let Name = "Nishant"
+let age = 18
+let LoggedIn = false
+
+//datatype 
+//number -> range 2 to power 53
+//bigint
+//string ->" "
+//boolean->true/false
+//null -> standalone value
+//undefined -> value has not been assigned yet
+//symbol -> unique
+//objects -> 
+
+console.log(typeof "Nishant");
+console.log(typeof null);//object
+console.log(typeof age);
+console.log(typeof undefined);//undefined
+
+
