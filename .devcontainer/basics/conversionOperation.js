af709f6422e4b54fcd40c6d8f71e37f11f33d40f
nish0753 <130308428+nish0753@users.noreply.github.com>
2024-06-03 06:31:06 +0000

    Conversion of Datatypes

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,30 @@
+let score ="Nishant"
+
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//'33'=> 33
+//"33abc"=> NaN Not a number
+//true ;->1 false => 0
+
+let isLoggedIn = "Nishant"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true. 0 => flase
+//"" => false
+//"Nishant" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber);
+
+console.log(typeof stringNumber)
