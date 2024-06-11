9157f8c22d784a6f868886d9c25d200196012e3d
nish0753 <130308428+nish0753@users.noreply.github.com>
2024-06-11 18:25:43 +0000

    Maths

---
+++ .devcontainer/nums&Maths.js
@@ -0,0 +1,30 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(10000);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+const otherNumber= 23.989;
+console.log(otherNumber.toPrecision(3));
+
+const hundred = 100000;
+console.log(hundred.toLocaleString('en-IN'));
+
+Number.MAX_SAFE_INTEGER;
+console.log(Number.MAX_SAFE_INTEGER)
+
+//+++++++++++++++MATHS +++++++++++++//
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.8));
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9));
+console.log(Math. sqrt(999));
+
+console.log(Math.floor(Math.random()));
+console.log(Math.floor((Math.random()*10)+1));
+console.log(Math.floor((Math.random()*10)+1));
